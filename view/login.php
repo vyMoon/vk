9aaf04bc7d198f96d74d6e47568b376345711998
vyMoon <46728118+vyMoon@users.noreply.github.com>
2019-02-19 23:00:15 +0300

    Create login.php

---
+++ view/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Авторизация</title>
+    </head>
+
+    <body>
+
+        <h2>Авторизация</h2>
+        <p>
+            <a href=" <?php echo $link; ?> ">авторизоваться в контакте</a>
+        </p>
+
+    </body>
+</html>
